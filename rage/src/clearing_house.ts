f8a5a14a2f8730a164e13f1bd331158f75b8fbdd
0xperp <0xperp@protonmail.com>
2022-01-28 07:57:02 -0500

    init: ClearingHouse function calls

---
+++ rage/src/clearing_house.ts
@@ -0,0 +1,93 @@
+// Mapping Events
+export function handleGovernanceTransferred(event: GovernanceTransferred): void {
+
+}
+
+export function handleTeamMultisigTransferred(event: TeamMultisigTransferred): void {
+
+}
+
+// Mapping Functions
+export function handleAddCollateralSupport(call: AddCollateralSupport): void {
+
+}
+
+export function handleAddMargin(call: AddMargin) {
+
+}
+
+export function handleCreateAccount(call: CreateAccount) {
+
+}
+
+export function handleInitRealToken(call: InitRealToken) {
+
+}
+
+export function handleLiquidateLiquidityPositions(call: LiquidateLiquidityPositions) {
+
+}
+
+export function handleLiquidateLiquidityPositionsWithGasClaim(call: LiquidateLiquidityPositionsWithGasClaim) {
+
+}
+
+export function handleLiquidateTokenPosition(call: LiquidateTokenPosition) {
+
+}
+
+export function handleRemoveLimitOrder(call: RemoveLimitOrder) {
+
+}
+
+export function handleRemoveLimitOrderWithGasClaim(call: RemoveLimitOrderWithGasClaim) {
+
+}
+
+export function handleRemoveMargin(call: RemoveMargin) {
+
+}
+
+export function handleRemoveProfit(call: RemoveProfit) {
+
+}
+
+export function handleSetPaused(calll: SetPaused) {
+
+}
+
+export function handleSetPlatformParameters(call: SetPlatformParameters) {
+
+}
+
+export function handleSwapToken(call: SwapToken) {
+
+}
+
+export function handleTransferGovernance(call: TransferGovernance) {
+
+}
+
+export function handleTransferTeamMultisig(call: TransferTeamMultisig) {
+
+}
+
+export function handleUpdateRageTradePoolSettings(call: UpdateRageTradePoolSettings) {
+
+}
+
+export function handleUpdateRangeOrder(call: UpdateRangeOrder) {
+
+}
+
+export function handleUpdateSupportedDeposits(call: UpdateSupportedDeposits) {
+
+}
+
+export function handleUpdateSupportedVTokens(call: UpdateSupportedVTokens) {
+
+}
+
+export function handleWithdrawProtocolFee(call: WithdrawProtocolFee) {
+
+}
