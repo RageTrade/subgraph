50e3962d4eebe7f9a7094a6549f1f62c0ac1821f
0xperp <0xperp@protonmail.com>
2022-01-27 21:59:19 -0500

    init: schema

--- rage/src/mapping.ts
+++ rage/src/mapping.ts
@@ -1,14 +1,51 @@
-import { NewGravatar, UpdatedGravatar } from '../generated/Gravity/Gravity'
-import { Gravatar } from '../generated/schema'
+// import { NewGravatar, UpdatedGravatar } from '../generated/Gravity/Gravity'
+// import { Gravatar } from '../generated/schema'
 
-export function handleProtocolFeeWithdrawn(event: UpdatedGravatar): void {
-  let id = event.params.id.toHex()
-  let gravatar = Gravatar.load(id)
-  if (gravatar == null) {
-    gravatar = new Gravatar(id)
-  }
-  gravatar.owner = event.params.owner
-  gravatar.displayName = event.params.displayName
-  gravatar.imageUrl = event.params.imageUrl
-  gravatar.save()
+export function handleAccountCreated(event: ProtocolFeeWithdrawn): void {
+
+}
+
+export function handleDepositMargin(event: DepositMargin): void {
+
+}
+
+export function handleFundingPayment(event: FundingPayment): void {
+
+}
+
+export function handleLiquidateRanges(event: handleLiquidateRanges): void {
+
+}
+
+export function hadleLiquidateTokenPositions(event: LiquidateTokenPosition): void {
+
+}
+
+export function handleLiquidityChange(event: LiquidateTokenPositionChange): void {
+
+}
+
+export function handleLiquidityFee(event: LiquidityFee): void {
+
+}
+
+export function handleLiquidityTokenPositionChange(event: LiquidityTokenPositionChange): void {
+
+}
+
+export function handleProtocolFeeWithdrawm(event: ProtocolFeeWithdrawm): void {
+
+}
+
+export function handleTokenPositionChange(event: TokenPositionChange): void {
+
+}
+
+export function handleWithdrawMargin(event: WithdrawMargin): void {
+
+}
+
+
+export function handleWithdrawProfit(event: WithdrawProfit): void {
+
 }
