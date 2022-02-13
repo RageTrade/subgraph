ff641a069cf3dcafe9e4dca7943fc0528f0f7f3a
0xperp <0xperp@protonmail.com>
2022-02-13 16:19:44 -0500

    feat: updated schema

--- rage/src/account.ts
+++ rage/src/account.ts
@@ -10,16 +10,16 @@ import {
   ProtocolFeeWithdrawm,
   TokenPositionChange,
   WithdrawMargin,
-  WithdrawProfit
+  UpdateProfit
 } from '../generated/AccountLibrary/AccountLibrary'
 import {
   Account,
-  TokenPosition,
-  RangePosition,
-  Protocol,
   Margin,
-  RangeLiquidation,
-  TokenLiquidation
+  TokenPosition,
+  LiquidateToken,
+  LiquidateRangePosition,
+  LiquidityPosition,
+  Protocol
 } from '../generated/schema'
 import { BigInt } from '@graphprotocol/graph-ts'
 
@@ -29,158 +29,236 @@ export function handleAccountCreated(event: AccountCreated): void {
 
   // nullable check see https://thegraph.com/docs/en/developer/assemblyscript-migration-guide/#nullability
   if (account) {
-      account.owner = event.params.ownerAddress
+      account.timestamp = event.block.timestamp
+      account.ownerAddress = event.params.ownerAddress
+      account.accountNo = event.params.accountNo
   }
 
-  // no need to include in nullable check since a new Accout is being created
+  // no need to include in nullable check since a new Account is being created
   // unless `new` is certain default to `load` see https://thegraph.com/docs/en/developer/create-subgraph-hosted/#writing-mappings
   account.save()
 }
 
 // @entity Margin
-export function handleWithdrawProfit(event: WithdrawProfit): void {
-  let account = Account.load(event.params.accountNo.toString())
+// export function handleWithdrawProfit(event: UpdateProfit): void {
+//   let time = event.block.timestamp
+//   let margin = new Margin(event.params.accountNo.toString() + "-" + time)
+//
+//   // nullable check
+//   if (account) {
+//       margin.timestamp = time
+//       margin.accountNo = event.params.accountNo
+//       margin.totalProfit += event.params.amount
+//       margin.save()
+//   }
+// }
+
+export function handleUpdateProfit(event: UpdateProfit): void {
+  let time = event.block.timestamp
+  let margin = new Margin(event.params.accountNo.toString() + "-" + time.toString())
 
   // nullable check
-  if (account) {
-      account.amount =  event.params.amount
-      account.save()
+  if (margin) {
+      margin.timestamp = time
+      margin.accountNo = event.params.accountNo
+      margin.totalProfit += event.params.amount
+      margin.save()
   }
 }
 
 // @entity Margin
 export function handleDepositMargin(event: DepositMargin): void {
-  let margin = Margin.load(event.params.accountNo.toString())
+  let time = event.block.timestamp
+  let margin = new Margin(event.params.accountNo.toString() + "-" + time.toString())
 
   // nullable check
   if (margin) {
-    margin.token = event.params.rTokenAddress
-    margin.amount = event.params.amount
+    margin.timestamp = time
+    margin.accountNo = event.params.accountNo
+    margin.rTokenAddress = event.params.rTokenAddress
+    margin.marginAmount = event.params.amount
     margin.save()
   }
 }
 
 // @entity Margin
 export function handleWithdrawMargin(event: WithdrawMargin): void {
-  let margin = Margin.load(event.params.accountNo.toString())
+  let time = event.block.timestamp
+  let margin = new Margin(event.params.accountNo.toString() + "-" + time.toString())
 
   // nullable check
   if (margin) {
-    margin.token = event.params.rTokenAddress
-    margin.amount = event.params.amount
+    margin.timestamp = time
+    margin.accountNo = event.params.accountNo
+    margin.rTokenAddress = event.params.rTokenAddress
+    margin.marginAmount = event.params.amount
     margin.save()
   }
 }
 
-// @entity RangePosition
+// @entity LiquidityPosition
 export function handleFundingPayment(event: FundingPayment): void {
-  let range = RangePosition.load(event.params.accountNo.toString())
+  let time = event.block.timestamp
+  let liquidityPosition = new LiquidityPosition(
+    event.params.accountNo.toString() + "-" +
+    event.params.vToken.toHexString() + "-" +
+    time.toString()
+  )
 
   // nullable check
-  if (range) {
-    range.token = event.params.vToken
-    range.tickLower = BigInt.fromI32(event.params.tickLower)
-    range.tickUpper = BigInt.fromI32(event.params.tickUpper)
-    range.amount = event.params.amount
-    range.save()
+  if (liquidityPosition) {
+    liquidityPosition.timestamp = time
+    liquidityPosition.accountNo = event.params.accountNo
+    liquidityPosition.vToken = event.params.vToken
+    liquidityPosition.tickLower = BigInt.fromI32(event.params.tickLower)
+    liquidityPosition.tickUpper = BigInt.fromI32(event.params.tickUpper)
+    liquidityPosition.fundingPayment = event.params.amount
+    liquidityPosition.save()
   }
 }
 
-// @entity RangeLiquidation
+// @entity LiquidateRanges
 export function handleLiquidateRanges(event: LiquidateRanges): void {
-  let liquidation = RangeLiquidation.load(event.params.accountNo.toString())
+  let time = event.block.timestamp
+  let liquidateRanges = new LiquidateRangePosition(
+    event.params.accountNo.toString() + "-" +
+    event.params.keeperAddress.toHexString() + "-" +
+    time.toString()
+  )
 
   // nullable check
-  if (liquidation) {
-    liquidation.keeperAddress = event.params.keeperAddress
-    liquidation.liquidationFee = event.params.liquidationFee
-    liquidation.keeperFee = event.params.keeperFee
-    liquidation.insuranceFundFee = event.params.insuranceFundFee
-    liquidation.save()
+  if (liquidateRanges) {
+    liquidateRanges.timestamp = time
+    liquidateRanges.accountNo = event.params.accountNo
+    liquidateRanges.keeperAddress = event.params.keeperAddress
+    liquidateRanges.liquidationFee = event.params.liquidationFee
+    liquidateRanges.keeperFee = event.params.keeperFee
+    liquidateRanges.insuranceFundFee = event.params.insuranceFundFee
+    liquidateRanges.save()
   }
 }
 
-// @entity RangePosition
-export function handleLiquidityChange(event: LiquidityChange): void {
-  let range = RangePosition.load(event.params.accountNo.toString())
-
-  // nullable check
-  if (range) {
-    range.token = event.params.vToken
-    range.tickLower = BigInt.fromI32(event.params.tickLower)
-    range.tickUpper = BigInt.fromI32(event.params.tickUpper)
-    range.liquidityDelta = event.params.liquidityDelta
-    range.limitOrderType = event.params.limitOrderType.toString()
-    range.tokenAmountOut = event.params.tokenAmountOut
-    range.baseAmountOut = event.params.baseAmountOut
-    range.save()
-  }
-}
-
-// @entity RangePosition
-export function handleLiquidityFee(event: LiquidityFee): void {
-  let range = RangePosition.load(event.params.accountNo.toString())
-
-  // nullable check
-  if (range) {
-    range.token = event.params.vToken
-    range.tickLower = BigInt.fromI32(event.params.tickLower)
-    range.tickUpper = BigInt.fromI32(event.params.tickUpper)
-    range.amount = event.params.amount
-    range.save()
-  }
-}
-
-// @entity RangePosition
-export function handleLiquidityTokenPositionChange(event: LiquidityTokenPositionChange): void {
-  let range = RangePosition.load(event.params.accountNo.toString())
-
-  // nullable check
-  if (range) {
-    range.token = event.params.vToken
-    range.tickLower = BigInt.fromI32(event.params.tickLower)
-    range.tickUpper = BigInt.fromI32(event.params.tickUpper)
-    range.tokenAmountOut = event.params.tokenAmountOut
-    range.save()
-  }
-}
-
-// @entity TokenPosition
+// @entity LiquidateTokenPosition
 export function handleLiquidateTokenPosition(event: LiquidateTokenPosition): void {
-  let liquidation = TokenLiquidation.load(event.params.accountNo.toString())
+  let time = event.block.timestamp
+  let liquidateTokenPosition = new LiquidateToken(
+    event.params.accountNo.toString() + "-" +
+    event.params.vToken.toHexString() + "-" +
+    time.toString()
+  )
 
   // nullable check
-  if (liquidation) {
-    liquidation.token = event.params.vToken
-    liquidation.liquidationBps = BigInt.fromI32(event.params.liquidationBps)
-    liquidation.liquidationPriceX128 = event.params.liquidationPriceX128
-    liquidation.liquidatorPriceX128 = event.params.liquidationPriceX128
-    liquidation.insuranceFundFee = event.params.insuranceFundFee
-    liquidation.save()
+  if (liquidateTokenPosition) {
+    liquidateTokenPosition.timestamp = time
+    liquidateTokenPosition.accountNo = event.params.accountNo
+    liquidateTokenPosition.liquidatorAccountNo = event.params.liquidatorAccountNo
+    liquidateTokenPosition.vToken = event.params.vToken
+    liquidateTokenPosition.liquidationBps = BigInt.fromI32(event.params.liquidationBps)
+    liquidateTokenPosition.liquidationPriceX128 = event.params.liquidationPriceX128
+    liquidateTokenPosition.liquidatorPriceX128 = event.params.liquidatorPriceX128
+    liquidateTokenPosition.insuranceFundFee = event.params.insuranceFundFee
+    liquidateTokenPosition.save()
   }
 }
 
-// @entity TokenPosition
-export function handleTokenPositionChange(event: TokenPositionChange): void {
-  let token = TokenPosition.load(event.params.accountNo.toString())
+// @entity LiquidityPosition
+export function handleLiquidityChange(event: LiquidityChange): void {
+  let time = event.block.timestamp
+  let liquidityPosition = new LiquidityPosition(
+    event.params.accountNo.toString() + "-" +
+    event.params.vToken.toHexString() + "-" +
+    time.toString()
+  )
 
   // nullable check
-  if (token) {
-    token.token = event.params.vToken
-    token.tokenAmountOut = event.params.tokenAmountOut
-    token.baseAmountOut = event.params.baseAmountOut
-    token.save()
+  if (liquidityPosition) {
+    liquidityPosition.timestamp = time
+    liquidityPosition.accountNo = event.params.accountNo
+    liquidityPosition.vToken = event.params.vToken
+    liquidityPosition.tickLower = BigInt.fromI32(event.params.tickLower)
+    liquidityPosition.tickUpper = BigInt.fromI32(event.params.tickUpper)
+    liquidityPosition.liquidityDelta = event.params.liquidityDelta
+    liquidityPosition.limitOrderType = event.params.limitOrderType.toString()
+    liquidityPosition.tokenAmountOut = event.params.tokenAmountOut
+    liquidityPosition.baseAmountOut = event.params.baseAmountOut
+    liquidityPosition.save()
+  }
+}
+
+// @entity LiquidityPosition
+export function handleLiquidityFee(event: LiquidityFee): void {
+  let time = event.block.timestamp
+  let liquidityPosition = new LiquidityPosition(
+    event.params.accountNo.toString() + "-" +
+    event.params.vToken.toHexString() + "-" +
+    time.toString()
+  )
+
+  // nullable check
+  if (liquidityPosition) {
+    liquidityPosition.timestamp = time
+    liquidityPosition.accountNo = event.params.accountNo
+    liquidityPosition.vToken = event.params.vToken
+    liquidityPosition.tickLower = BigInt.fromI32(event.params.tickLower)
+    liquidityPosition.tickUpper = BigInt.fromI32(event.params.tickUpper)
+    liquidityPosition.tokenAmountOut = event.params.amount
+    liquidityPosition.save()
+  }
+}
+
+// @entity LiquidityPosition
+export function handleLiquidityTokenPositionChange(event: LiquidityTokenPositionChange): void {
+  let time = event.block.timestamp
+  let liquidityPosition = new LiquidityPosition(
+    event.params.accountNo.toString() + "-" +
+    event.params.vToken.toHexString() + "-" +
+    time.toString()
+  )
+
+  // nullable check
+  if (liquidityPosition) {
+    liquidityPosition.timestamp = time
+    liquidityPosition.accountNo = event.params.accountNo
+    liquidityPosition.vToken = event.params.vToken
+    liquidityPosition.tickLower = BigInt.fromI32(event.params.tickLower)
+    liquidityPosition.tickUpper = BigInt.fromI32(event.params.tickUpper)
+    liquidityPosition.tokenAmountOut = event.params.tokenAmountOut
+    liquidityPosition.save()
   }
 }
 
 // @entity Protocol
 export function handleProtocolFeeWithdrawm(event: ProtocolFeeWithdrawm): void {
-  let protocol = Protocol.load(event.params.wrapperAddress.toString())
+  let time = event.block.timestamp
+  let protocol = new Protocol(
+    event.params.wrapperAddress.toHexString()
+  )
 
   // nullable check
-  if (protocol) {
-    protocol.wrapperAddress = event.params.wrapperAddress.toString()
+  if(protocol) {
+    protocol.timestamp = time
+    protocol.wrapperAddress = event.params.wrapperAddress
     protocol.feeAmount = event.params.feeAmount
     protocol.save()
+  }
+}
+
+// @entity TokenPosition
+export function handleTokenPositionChange(event: TokenPositionChange): void {
+  let time = event.block.timestamp
+  let tokenPosition = new TokenPosition(
+    event.params.accountNo.toString() + "-" +
+    event.params.vToken.toHexString() + "-" +
+    time.toString()
+  )
+
+  // nullable check
+  if (tokenPosition) {
+    tokenPosition.timestamp = time
+    tokenPosition.accountNo = event.params.accountNo
+    tokenPosition.vToken = event.params.vToken
+    tokenPosition.tokenAmountOut = event.params.tokenAmountOut
+    tokenPosition.baseAmountOut = event.params.baseAmountOut
+    tokenPosition.save()
   }
 }
