cbb0a3d1f68efc1ee0032a79b69c6505c7f8b6c6
fr0ntenddev <fr0ntenddev@rage.trade>
2022-03-05 16:48:40 +0530

    fix: switch from vTokenAddress to poolId

--- rage/src/mappings/clearinghouse/token-position.ts
+++ rage/src/mappings/clearinghouse/token-position.ts
@@ -1,20 +1,23 @@
-import { Address, BigInt, log } from '@graphprotocol/graph-ts';
+import { BigInt, log } from '@graphprotocol/graph-ts';
 import { Account, TokenPosition } from '../../../generated/schema';
 import { generateId } from '../../utils';
-import { generateAccountId } from './account';
 
 export function getTokenPosition(
   account: Account,
-  vTokenAddress: Address
+  poolId: BigInt
 ): TokenPosition {
-  let tokenPositionId = generateId([account.id, vTokenAddress.toHexString()]);
+  let tokenPositionId = generateId([account.id, poolId.toHexString()]);
+  log.debug(
+    'custom_logs: handlePoolInitialized poolId and tokenPositionId {}',
+    [poolId.toHexString(), tokenPositionId]
+  );
 
   let tokenPosition = TokenPosition.load(tokenPositionId);
   if (tokenPosition === null) {
     // creating empty object
     tokenPosition = new TokenPosition(tokenPositionId);
     tokenPosition.account = account.id;
-    tokenPosition.vToken = vTokenAddress;
+    tokenPosition.rageTradePool = poolId.toHexString();
     tokenPosition.netPosition = BigInt.fromI32(0);
     tokenPosition.save();
   }
