abdee8490198f4af0b49f8c952fc8ce647ed2f9f
fr0ntenddev <fr0ntenddev@rage.trade>
2022-03-08 23:22:37 +0530

    fix: added new collection fields and updated schema

--- rage/src/mappings/ragetradeFactory/handler.ts
+++ rage/src/mappings/ragetradeFactory/handler.ts
@@ -1,11 +1,13 @@
 import { log } from '@graphprotocol/graph-ts';
 import { PoolInitialized } from '../../../generated/RageTradeFactory/RageTradeFactory';
 import {
+  Collection,
   RageTradeFactory,
   RageTradePool,
+  VPoolWrapper,
   VToken,
 } from '../../../generated/schema';
-import { getSumAX128, truncate } from '../../utils';
+import { generateId, truncate } from '../../utils';
 import { ZERO_BD, ZERO_BI } from '../../utils/constants';
 
 export function handlePoolInitialized(event: PoolInitialized): void {
@@ -20,6 +22,8 @@ export function handlePoolInitialized(event: PoolInitialized): void {
     rageTradeFactory = new RageTradeFactory(event.address.toHexString());
     rageTradeFactory.save();
   }
+
+  let vPoolWrapper = new VPoolWrapper(event.params.vPoolWrapper.toHexString());
 
   let poolId = truncate(event.params.vToken.toHexString());
   log.debug('custom_logs: handlePoolInitialized poolId {}', [poolId]);
@@ -36,20 +40,29 @@ export function handlePoolInitialized(event: PoolInitialized): void {
   vToken.pool = poolId;
   vToken.save();
 
+  vPoolWrapper.pool = poolId;
+  vPoolWrapper.save();
+
   rageTradePool = new RageTradePool(poolId);
   rageTradePool.vToken = vToken.id;
   rageTradePool.vPool = event.params.vPool.toHexString();
-  rageTradePool.vPoolWrapper = event.params.vPoolWrapper.toHexString();
+  rageTradePool.vPoolWrapper = vPoolWrapper.id;
   rageTradePool.factory = rageTradeFactory.id;
+  rageTradePool.tick = ZERO_BI;
 
+  let dayCollection = new Collection(generateId([rageTradePool.id, 'dayData']));
+  let hourCollection = new Collection(
+    generateId([rageTradePool.id, 'hourData'])
+  );
+
+  rageTradePool.dayData = dayCollection.id;
+  rageTradePool.hourData = hourCollection.id;
 
   rageTradePool.sumAX128 = ZERO_BI;
   rageTradePool.price = ZERO_BD;
   rageTradePool.liquidity = ZERO_BI;
-  rageTradePool.funding = ZERO_BI;
   rageTradePool.volume24H = ZERO_BI;
   rageTradePool.priceChange24H = ZERO_BI;
-  rageTradePool.funding1H = ZERO_BI;
 
   rageTradePool.save();
 }
