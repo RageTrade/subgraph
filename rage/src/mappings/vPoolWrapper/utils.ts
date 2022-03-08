652af94a51654edfa1c719f31de41716761aaa16
fr0ntenddev <fr0ntenddev@rage.trade>
2022-03-08 23:21:41 +0530

    fix: update getCandle  and getPriceANDTick function

--- rage/src/mappings/vPoolWrapper/utils.ts
+++ rage/src/mappings/vPoolWrapper/utils.ts
@@ -1,17 +1,20 @@
 import { Candle, RageTradePool } from '../../../generated/schema';
 import { ZERO_BD, ZERO_BI } from '../../utils/constants';
-import { BigInt } from '@graphprotocol/graph-ts';
+import { Address, BigDecimal, BigInt, log } from '@graphprotocol/graph-ts';
+import { Pool } from '../../../generated/templates/Pool/Pool';
+import { parseSqrtPriceX96 } from '../../utils';
 
 export function getCandle(
   id: string,
   periodStartUnix: BigInt,
-  rageTradePool: RageTradePool
+  collectionId: string,
+  initialPrice: BigDecimal
 ): Candle {
   let candle = Candle.load(id);
 
   if (candle === null) {
     candle = new Candle(id);
-    candle.rageTradePool = rageTradePool.id;
+    candle.collection = collectionId;
     candle.periodStartUnix = periodStartUnix;
 
     candle.volumeVToken = ZERO_BD; // token0
@@ -21,11 +24,49 @@ export function getCandle(
     candle.sumBX128 = ZERO_BI;
     candle.sumFpX128 = ZERO_BI;
     candle.sumFeeX128 = ZERO_BI;
-    candle.open = rageTradePool.price;
-    candle.high = rageTradePool.price;
-    candle.low = rageTradePool.price;
-    candle.close = rageTradePool.price;
+
+    candle.open = initialPrice;
+    candle.high = initialPrice;
+    candle.low = initialPrice;
+    candle.close = initialPrice;
+
+    candle.save();
   }
 
-  return candle;
+  return candle as Candle;
+}
+
+class PriceANDTick {
+  price: BigDecimal;
+  tick: BigInt;
+}
+
+/**
+ *
+ * @param vPoolAddress
+ * @returns object with price and tick
+ * ```ts
+ * class PriceANDTick {
+ *   price: BigDecimal;
+ *   tick: BigInt;
+ * }
+ * ```
+ */
+export function getPriceANDTick(vPoolAddress: Address): PriceANDTick {
+  // Pool is UniswapV3Pool
+  let slot_result = Pool.bind(vPoolAddress).try_slot0();
+
+  let data = new PriceANDTick();
+
+  if (!slot_result.reverted) {
+    data.price = parseSqrtPriceX96(slot_result.value.value0);
+    data.tick = BigInt.fromI32(slot_result.value.value1);
+  } else {
+    log.error('priceStr reverted in getPriceANDTick', ['']);
+
+    data.price = ZERO_BD;
+    data.tick = ZERO_BI;
+  }
+
+  return data;
 }
