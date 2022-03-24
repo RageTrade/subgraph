0e0430b370a1f063a376e7216826dbff72631565
fr0ntenddev <fr0ntenddev@rage.trade>
2022-03-24 17:49:49 +0530

    feat: new getRageTradePool initializer function

---
+++ rage/src/mappings/ragetradeFactory/rageTradePool.ts
@@ -0,0 +1,43 @@
+import { BigInt, log } from '@graphprotocol/graph-ts';
+import { RageTradePool } from '../../../generated/schema';
+import { ZERO_BD, ZERO_BI } from '../../utils/constants';
+
+export function getRageTradePoolId(poolId: BigInt): string {
+  return poolId.toHexString();
+}
+
+export function getRageTradePool(poolId: string) {
+  let rageTradePool = RageTradePool.load(poolId);
+
+  if (rageTradePool === null) {
+    log.warning('custom_logs: getRageTradePool rageTradePool does not exist [[ poolId - {} ]]', [poolId]);
+
+    rageTradePool = new RageTradePool(poolId);
+
+    // empty values, overridden in handlePoolInitialized
+    rageTradePool.vToken = 'vToken.id';
+    rageTradePool.vPool = 'vPool.id';
+    rageTradePool.vPoolWrapper = 'vPoolWrapper.id';
+    rageTradePool.factory = 'rageTradeFactory.id';
+
+    rageTradePool.price = ZERO_BD;
+    rageTradePool.tick = ZERO_BI;
+    rageTradePool.liquidity = ZERO_BI;
+
+    rageTradePool.vTotalValueLocked = ZERO_BD;
+    rageTradePool.maintenanceMarginRatioBps = ZERO_BD;
+
+    rageTradePool.fundingRate = ZERO_BI;
+    rageTradePool.volume24H = ZERO_BI;
+    rageTradePool.priceChange24H = ZERO_BI;
+
+    rageTradePool.sumAX128 = ZERO_BI;
+    rageTradePool.sumBX128 = ZERO_BI;
+    rageTradePool.sumFpX128 = ZERO_BI;
+    rageTradePool.sumFeeX128 = ZERO_BI;
+
+    rageTradePool.save();
+  }
+
+  return rageTradePool as RageTradePool;
+}
