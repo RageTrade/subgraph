c9d3d9ef3e4044b5469ac3b21e5082fe87396945
fr0ntenddev <fr0ntenddev@rage.trade>
2022-03-29 17:35:04 +0530

    feat: addd new BI_6 constatnt

--- rage/src/utils/constants.ts
+++ rage/src/utils/constants.ts
@@ -9,3 +9,5 @@ export let ONE_BI = BigInt.fromI32(1);
 export let ZERO_BD = BigDecimal.fromString('0');
 export let ONE_BD = BigDecimal.fromString('1');
 export let BI_18 = BigInt.fromI32(18);
+export let BI_6 = BigInt.fromI32(6);
+
