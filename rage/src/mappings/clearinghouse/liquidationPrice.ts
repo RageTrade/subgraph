5502b04fac2f232380cceb20fec1aa154e21e016
fr0ntenddev <fr0ntenddev@rage.trade>
2022-05-11 19:11:46 +0530

    fix: change signature of getLiquidationPrice

--- rage/src/mappings/clearinghouse/liquidationPrice.ts
+++ rage/src/mappings/clearinghouse/liquidationPrice.ts
@@ -1,53 +1,46 @@
-import { BigDecimal, BigInt, log } from '@graphprotocol/graph-ts';
-import {
-  Account,
-  RageTradePool,
-  TokenPosition,
-} from '../../../generated/schema';
+import { BigDecimal, log } from '@graphprotocol/graph-ts';
+
 import { safeDiv } from '../../utils';
-import { ZERO_BD, ZERO_BI } from '../../utils/constants';
+import { ZERO_BD } from '../../utils/constants';
 
 export function getLiquidationPrice(
-  tokenPosition: TokenPosition,
-  account: Account,
-  rageTradePool: RageTradePool,
+  netPosition: BigDecimal, // TokenPosition
+  vQuoteBalance: BigDecimal, // Account
+  marginBalance: BigDecimal, // Account
+  maintenanceMarginRatioBps: BigDecimal // RageTradePool
 ): BigDecimal {
   let tenPow4 = BigDecimal.fromString('10000');
 
   let liquidationPrice = ZERO_BD;
 
-  if (tokenPosition.netPosition.gt(ZERO_BD)) {
+  if (netPosition.gt(ZERO_BD)) {
     // Liquidation Price (Long Position) = - (vQuoteBalance + marginAmount)*1e4/(netPosition * (1e4 -maintenanceMarginRatioBps ))
     liquidationPrice = safeDiv(
-      account.vQuoteBalance
-        .plus(account.marginBalance) 
+      vQuoteBalance
+        .plus(marginBalance)
         .times(tenPow4)
         .neg(),
-      tokenPosition.netPosition.times(
-        tenPow4.minus(rageTradePool.maintenanceMarginRatioBps)
-      )
+      netPosition.times(tenPow4.minus(maintenanceMarginRatioBps))
     );
   } else {
     // Liquidation Price (Short Position) = - (vQuoteBalance + marginAmount)*1e4/netPosition(1e4+maintenanceMarginRatioBps)
     liquidationPrice = safeDiv(
-      account.vQuoteBalance
-        .plus(account.marginBalance)
+      vQuoteBalance
+        .plus(marginBalance)
         .times(tenPow4)
         .neg(),
-      tokenPosition.netPosition.times(
-        tenPow4.plus(rageTradePool.maintenanceMarginRatioBps)
-      )
+      netPosition.times(tenPow4.plus(maintenanceMarginRatioBps))
     );
   }
 
   log.debug(
-    'custom_logs: tokenPosition.liquidationPrice [liquidationPrice - {}] [vQuoteBalance - {}] [marginBalance - {}] [netPosition - {}] [maintenanceMarginRatioBps - {}]',
+    'custom_logs: liquidationPrice [liquidationPrice - {}] [vQuoteBalance - {}] [marginBalance - {}] [netPosition - {}] [maintenanceMarginRatioBps - {}]',
     [
       liquidationPrice.toString(),
-      account.vQuoteBalance.toString(),
-      account.marginBalance.toString(),
-      tokenPosition.netPosition.toString(),
-      rageTradePool.maintenanceMarginRatioBps.toString(),
+      vQuoteBalance.toString(),
+      marginBalance.toString(),
+      netPosition.toString(),
+      maintenanceMarginRatioBps.toString(),
     ]
   );
 
