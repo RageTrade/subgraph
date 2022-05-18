ace022b85192585a72c5d22903e9e8101e9eef35
fr0ntenddev <fr0ntenddev@rage.trade>
2022-05-18 11:26:22 +0530

    fix: empty handler functions for handleDeposit, handleWithdraw

---
+++ rage/src/mappings/CurveYieldStrategy/handler.ts
@@ -0,0 +1,5 @@
+import { Deposit, Withdraw } from "../../../generated/CurveYieldStrategy/CurveYieldStrategy";
+
+export function handleDeposit(event: Deposit): void {}
+export function handleWithdraw(event: Withdraw): void {}
+
