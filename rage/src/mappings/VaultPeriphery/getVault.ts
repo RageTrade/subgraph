6c9512df87955c316decc1dfaeb4d6ee6c4ce03a
fr0ntenddev <fr0ntenddev@rage.trade>
2022-05-18 17:04:57 +0530

    feat: added getVaults function

---
+++ rage/src/mappings/VaultPeriphery/getVault.ts
@@ -0,0 +1,15 @@
+import { Address } from '@graphprotocol/graph-ts';
+import { Vault } from '../../../generated/schema';
+
+export function getVault(vaultAddress: Address): Vault {
+  let vaultId = vaultAddress.toHexString();
+  let vault = Vault.load(vaultId);
+
+  if (vault == null) {
+    vault = new Vault(vaultId);
+
+    vault.save();
+  }
+
+  return vault;
+}
