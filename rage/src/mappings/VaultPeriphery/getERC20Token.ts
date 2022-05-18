3b4234f679e68ca7e3d468bcd1ad82f9ec2af891
fr0ntenddev <fr0ntenddev@rage.trade>
2022-05-18 17:05:07 +0530

    feat: added getERC20Token function

---
+++ rage/src/mappings/VaultPeriphery/getERC20Token.ts
@@ -0,0 +1,24 @@
+import { Address } from '@graphprotocol/graph-ts';
+import { ERC20Token } from '../../../generated/schema';
+import {
+  fetchTokenDecimals,
+  fetchTokenName,
+  fetchTokenSymbol,
+} from '../../utils/token';
+
+export function getERC20Token(tokenAddress: Address): ERC20Token {
+  let tokenId = tokenAddress.toHexString();
+
+  let token = ERC20Token.load(tokenId);
+
+  if (token == null) {
+    token = new ERC20Token(tokenId);
+    token.symbol = fetchTokenSymbol(tokenAddress);
+    token.name = fetchTokenName(tokenAddress);
+    token.decimals = fetchTokenDecimals(tokenAddress);
+
+    token.save();
+  }
+
+  return token;
+}
