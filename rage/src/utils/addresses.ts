a9772595dcb24eeec422734add87d716cb280885
fr0ntenddev <fr0ntenddev@rage.trade>
2022-05-18 20:56:09 +0530

    chore: codegen

--- rage/src/utils/addresses.ts
+++ rage/src/utils/addresses.ts
@@ -7,6 +7,7 @@ class Contracts {
   VPoolWrapper: Address;
   CurveYieldStrategy: Address;
   VaultPeriphery: Address;
+  CurveTriCryptoLpTokenAddress: Address;
 }
 
 export let contracts: Contracts = { 
@@ -15,5 +16,6 @@ export let contracts: Contracts = {
   InsuranceFund: Address.fromString("0xA2423321c854a37F002D3c276DE1CA9E88935bf2"),
   VPoolWrapper: Address.fromString("0x2A13e9fE24fd6aa1736120Dc83138E3c182551FB"),
   CurveYieldStrategy: Address.fromString("0xAC81Ec44483CD8D36A62a5d88a7C4C93967BA0e4"),
-  VaultPeriphery: Address.fromString("0x212f250163B890198B382223b8825B7875f9B790")
+  VaultPeriphery: Address.fromString("0x212f250163B890198B382223b8825B7875f9B790"),
+  CurveTriCryptoLpTokenAddress: Address.fromString("0xeA78216e422A623aF359D69d6Ef88B703F15a78B")
  };
