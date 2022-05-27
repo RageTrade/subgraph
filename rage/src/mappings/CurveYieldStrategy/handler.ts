6ab3c1a6d2b2c8dc2514202709880829465111d9
fr0ntenddev <fr0ntenddev@rage.trade>
2022-05-27 20:43:12 +0530

    feat: handleDeposit for CurveYieldStrategy

--- rage/src/mappings/CurveYieldStrategy/handler.ts
+++ rage/src/mappings/CurveYieldStrategy/handler.ts
@@ -12,7 +12,82 @@ import { getOwner } from '../clearinghouse/owner';
 import { getERC20Token } from '../VaultPeriphery/getERC20Token';
 import { getVault } from '../VaultPeriphery/getVault';
 
-export function handleDeposit(event: Deposit): void {}
+export function handleDeposit(event: Deposit): void {
+  log.debug(
+    'custom_logs: handleDeposit triggered [ caller - {} ] [ owner - {} ] [ asset - {} ] [ shares - {} ]',
+    [
+      event.params.caller.toHexString(),
+      event.params.owner.toHexString(),
+      event.params.assets.toString(),
+      event.params.shares.toString(),
+    ]
+  );
+
+  //...........................................................................//
+
+  if (
+    contracts.VaultPeriphery.toHexString() == event.params.caller.toHexString()
+  ) {
+    log.error(
+      'custom_logs: handleDeposit event came from VaultPeriphery - {} | caller - {}',
+      [
+        contracts.VaultPeriphery.toHexString(),
+        event.params.caller.toHexString(),
+      ]
+    );
+    return;
+  }
+
+  let vault = getVault(contracts.CurveYieldStrategy);
+  let owner = getOwner(event.params.owner);
+  let token = getERC20Token(contracts.CurveTriCryptoLpTokenAddress);
+
+  let curveYieldStrategyContract = CurveYieldStrategy.bind(
+    contracts.CurveYieldStrategy
+  );
+
+  let vaultDepositWithdrawEntryId = generateId([
+    vault.id,
+    owner.id,
+    event.params.owner.toHexString(),
+    event.block.number.toHexString(),
+    event.logIndex.toHexString(),
+  ]);
+
+  let entry = new VaultDepositWithdrawEntry(vaultDepositWithdrawEntryId);
+
+  entry.timestamp = event.block.timestamp;
+  entry.transactionHash = event.transaction.hash;
+
+  entry.owner = owner.id;
+  entry.vault = vault.id;
+  entry.token = token.id;
+
+  entry.action = 'deposit';
+
+  let assetPriceResult = curveYieldStrategyContract.try_getPriceX128();
+
+  if (assetPriceResult.reverted) {
+    log.error('custom_logs: getPriceX128 handleDepositPeriphery reverted {}', [
+      '',
+    ]);
+    return;
+  }
+
+  entry.assetsTokenAmount = BigIntToBigDecimal(event.params.assets, BI_18);
+  entry.tokenAmount = entry.assetsTokenAmount;
+
+  entry.sharesTokenAmount = BigIntToBigDecimal(event.params.shares, BI_18);
+
+  let priceOfAsset = parsePriceX128(assetPriceResult.value, BI_18, BI_6);
+  entry.sharesTokenDollarValue = entry.assetsTokenAmount.times(priceOfAsset);
+
+  owner.vaultDepositWithdrawEntriesCount = owner.vaultDepositWithdrawEntriesCount.plus(
+    ONE_BI
+  );
+  owner.save();
+  entry.save();
+}
 
 export function handleWithdraw(event: Withdraw): void {
   log.debug(
