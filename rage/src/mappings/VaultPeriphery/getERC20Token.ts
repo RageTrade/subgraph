1ff99874bf38119e34bde440cc59f44035b4d468
fr0ntenddev <fr0ntenddev@rage.trade>
2022-06-04 13:59:53 +0530

    chore: handle eth as ERC20 token

--- rage/src/mappings/VaultPeriphery/getERC20Token.ts
+++ rage/src/mappings/VaultPeriphery/getERC20Token.ts
@@ -1,5 +1,6 @@
-import { Address } from '@graphprotocol/graph-ts';
+import { Address, BigInt } from '@graphprotocol/graph-ts';
 import { ERC20Token } from '../../../generated/schema';
+import { ADDRESS_ZERO } from '../../utils/constants';
 import {
   fetchTokenDecimals,
   fetchTokenName,
@@ -12,10 +13,18 @@ export function getERC20Token(tokenAddress: Address): ERC20Token {
   let token = ERC20Token.load(tokenId);
 
   if (token == null) {
-    token = new ERC20Token(tokenId);
-    token.symbol = fetchTokenSymbol(tokenAddress);
-    token.name = fetchTokenName(tokenAddress);
-    token.decimals = fetchTokenDecimals(tokenAddress);
+    if (tokenAddress.equals(Address.fromString(ADDRESS_ZERO))) {
+      // special case for eth
+      token = new ERC20Token(ADDRESS_ZERO);
+      token.symbol = 'ETH';
+      token.name = 'Ether';
+      token.decimals = BigInt.fromI32(18);
+    } else {
+      token = new ERC20Token(tokenId);
+      token.symbol = fetchTokenSymbol(tokenAddress);
+      token.name = fetchTokenName(tokenAddress);
+      token.decimals = fetchTokenDecimals(tokenAddress);
+    }
 
     token.save();
   }
