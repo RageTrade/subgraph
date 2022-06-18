b3f2f945a1d07e12533871eac39bc6339ad3dfab
LeadDev <leaddev@anon.farm>
2022-06-18 14:49:44 +0530

    Patch abis

--- rage/update-subgraph.js
+++ rage/update-subgraph.js
@@ -62,15 +62,15 @@ async function main() {
   );
 
   // STEP 1: Copy ABIs
-  await copyAbi(rageTradeFactory, 'RageTradeFactory');
-  await copyAbi(clearingHouse, 'ClearingHouse');
-  await copyAbi(clearingHouseLens, 'ClearingHouseLens');
-  await copyAbi(insuranceFund, 'InsuranceFund');
-  await copyAbi(vPoolWrapperLogic, 'VPoolWrapperLogic');
-  await copyAbi(curveYieldStrategy, 'CurveYieldStrategy');
-  await copyAbi(vaultPeriphery, 'VaultPeriphery');
-  await copyAbi(crv3, 'CurveTriCryptoLpToken');
-  await copyAbi(quoter, 'CurveQuoter');
+  // await copyAbi(rageTradeFactory, 'RageTradeFactory');
+  // await copyAbi(clearingHouse, 'ClearingHouse');
+  // await copyAbi(clearingHouseLens, 'ClearingHouseLens');
+  // await copyAbi(insuranceFund, 'InsuranceFund');
+  // await copyAbi(vPoolWrapperLogic, 'VPoolWrapperLogic');
+  // await copyAbi(curveYieldStrategy, 'CurveYieldStrategy');
+  // await copyAbi(vaultPeriphery, 'VaultPeriphery');
+  // await copyAbi(crv3, 'CurveTriCryptoLpToken');
+  // await copyAbi(quoter, 'CurveQuoter');
 
   // STEP 2: Update ClearingHouse and other contract address in subgraph.yaml
   const subgraphYaml = yaml.parse(fs.readFileSync('./subgraph.yaml', 'utf8'));
@@ -138,9 +138,9 @@ function updateSubgraphYaml(
 }
 
 async function copyAbi(contract, name) {
-  const abi = contract.interface.fragments.map(f =>
-    JSON.parse(f.format('json'))
-  );
+  const abi = contract.interface.fragments
+    .map(f => JSON.parse(f.format('json')))
+    .filter(f => f.type !== 'error');
   await fs.writeJSON(`./abis/${name}.json`, abi, { spaces: 2 });
 
   console.log(`Updated ${name}.json`);
