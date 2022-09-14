b9253b4fdd05d6c8fd7faefbb31fddab8173e49a
fr0ntenddev <fr0ntenddev@rage.trade>
2022-09-14 18:14:02 +0530

    fixs: update configs

--- rage/src/utils/addresses.ts
+++ rage/src/utils/addresses.ts
@@ -1,4 +1,4 @@
-import { Address } from '@graphprotocol/graph-ts';
+import { Address } from '@graphprotocol/graph-ts'
 
 class Contracts {
   ClearingHouse: Address;
@@ -16,40 +16,18 @@ class Contracts {
   sGLP: Address;
 }
 
-export let contracts: Contracts = {
-  ClearingHouse: Address.fromString(
-    '0x979722E4E83E99378E89060462c8Fb16255ad45f'
-  ),
-  clearingHouseLens: Address.fromString(
-    '0x66941E71fA963B046Dd1E90F4a829Ec862B3BC75'
-  ),
-  RageTradeFactory: Address.fromString(
-    '0xc9c2083662Bf12160D6D60C40AB5889A26B0D09c'
-  ),
-  InsuranceFund: Address.fromString(
-    '0x571f0843B63D257745E1c75919B29aCa02C8F841'
-  ),
-  VPoolWrapper: Address.fromString(
-    '0x315E4BbB6D42e4288aCF72C7A9fb4a6A28A99ee6'
-  ),
-  CurveYieldStrategy: Address.fromString(
-    '0xD83025B6Ab3e4501967280c3C963b9a1E3E3C865'
-  ),
-  VaultPeriphery: Address.fromString(
-    '0x0AE60cF310a4FB66614D80Da1353E498EC989597'
-  ),
-  CurveTriCryptoLpTokenAddress: Address.fromString(
-    '0x931073e47baA30389A195CABcf5F3549157afdc9'
-  ),
-  CurveQuoter: Address.fromString('0x07E837cAbcC37A8b185051Ae0E984346CECde049'),
-  GMXYieldStrategy: Address.fromString(
-    '0x8fB1b11A573C46742a5a2A7b50C68518070069D4'
-  ),
-  GMXBatchingManager: Address.fromString(
-    '0xddA28C867641894dC79e152fd1673BCBa1025C2E'
-  ),
-  GlpStakingManager: Address.fromString(
-    '0xd0c350dc3E992DdF39Ea471ED7F2c579E935352b'
-  ),
-  sGLP: Address.fromString('0xfa14956e27D55427f7E267313D1E12d2217747e6'),
-};
+export let contracts: Contracts = { 
+  ClearingHouse: Address.fromString("0xe3B8eF0C2Ed6d8318F0b1b50A072e0cB508CDB04"),
+  clearingHouseLens: Address.fromString("0xFAEb252014eaE82e7954E33B16CCdbd058a1D89F"),
+  RageTradeFactory: Address.fromString("0x172b070dc24D8f0a3Cd665e601a398419c5272E6"),
+  InsuranceFund: Address.fromString("0xb84f7ACe7a6a5e54F84824605f7539aeD4A3e871"),
+  VPoolWrapper: Address.fromString("0x80Fe2ca9c7D5e77934CdAaF6f2eAe002991cb679"),
+  CurveYieldStrategy: Address.fromString("0xA1e33ebDA7e0bcC8d078886dA65d1Ea555495CEF"),
+  VaultPeriphery: Address.fromString("0x15023878aa94CdC9B9CE4A46919330B0537caCf5"),
+  CurveTriCryptoLpTokenAddress: Address.fromString("0x931073e47baA30389A195CABcf5F3549157afdc9"),
+  CurveQuoter: Address.fromString("0x07E837cAbcC37A8b185051Ae0E984346CECde049"),
+  GMXYieldStrategy: Address.fromString("0x71eec092b5Fccb12582f31993a3a401952239fe9"),
+  GMXBatchingManager: Address.fromString("0x21Cb40D3CD26b7E98BC449Fd68E23B2b431EE9f3"),
+  GlpStakingManager: Address.fromString("0x39aC75b66153Be03025E109f748928c812b23Afb"),
+  sGLP: Address.fromString("0xe2f057A1F5A1F100b9bF991709432f89602eAC68")
+ };
