b712bdc751890bd2000aa13ff6e4b09e8fda79e6
fr0ntenddev <fr0ntenddev@rage.trade>
2022-09-30 09:11:57 +0530

    checkpoint: arb-goerli deployment

--- rage/src/utils/addresses.ts
+++ rage/src/utils/addresses.ts
@@ -19,39 +19,39 @@ class Contracts {
 
 export let contracts: Contracts = {
   ClearingHouse: Address.fromString(
-    '0xA75EFa09cFD4c85380047Bb155D9314ADDF9cdE5'
+    '0xDBBa38B1bE4faAb8160dE5b8f749683588f8a12d'
   ),
   clearingHouseLens: Address.fromString(
-    '0xCD1a47E3844741446243238eE40dd9409bB17C3e'
+    '0x107135d67f39B9bd137AFcCAa2452ce89A03cA76'
   ),
   RageTradeFactory: Address.fromString(
-    '0xdAf61e4BbE5Ec32037826FF3eDdAbED6be1AF369'
+    '0x701c06135b0F51c2cbb35fB6C8c85e030f90Dc4B'
   ),
   InsuranceFund: Address.fromString(
-    '0x2086eD3639f3126F85629F91AD9c8fEE16d0D733'
+    '0x82187DE339d302169CeF6D0184424a9670be9796'
   ),
   VPoolWrapper: Address.fromString(
-    '0x1525b7257211b6500Fd310fF88772ED18Fb36Ff7'
+    '0x50fAf69a6483b923DD9DA33a798A05aF08550Da5'
   ),
   USDC: Address.fromString('0xa94D611f3b0FDBb0a10CE03DC2af9F9eae23D838'),
   CurveYieldStrategy: Address.fromString(
-    '0x7a18CD0Efd4D180ba00211ACed24632f1C1d4eB2'
+    '0x13aDde92e65eDF332a3437baA97175f8A55C5600'
   ),
   VaultPeriphery: Address.fromString(
-    '0x06d3BBC37A8CeB52c79BCb03854ba381d0FF930c'
+    '0x69e73B8c007fAF6BC90bf1b198Fa4D797D3Cb4Ea'
   ),
   CurveTriCryptoLpTokenAddress: Address.fromString(
-    '0xB642fE1abE33D2B1F58EcdfF1617fD10Cd20D5f7'
+    '0xd5981b565E66e57AE8A92eD110483a8EC68667C6'
   ),
-  CurveQuoter: Address.fromString('0x16A4f7E81E02f08c5FCb5282ABE04186825817Cb'),
+  CurveQuoter: Address.fromString('0xD543d34b3a62Ba9f18b0A1A4dEE2D98972F28c6D'),
   GMXYieldStrategy: Address.fromString(
-    '0x8753c1DA76da134b132F543338FEb26D948A37c6'
+    '0x1AB0182daB5FE245F32e59599C99d21675091634'
   ),
   GMXBatchingManager: Address.fromString(
-    '0xa9A13AD1eB114827735caE5390826055293abC17'
+    '0xBa67C8894fC10af6Ddd33cD9304D218B94D8DFd5'
   ),
   GlpStakingManager: Address.fromString(
-    '0xd4866df601CA85d33DF2453Fc83AFE996B26B8c1'
+    '0x726E1b7a46159F30FB4dA46c9625004044356813'
   ),
   sGLP: Address.fromString('0x1Dac875be521199a3dD6CC10e7e738122d11059C'),
 };
