cd55bc7903330d291ad07fc6bfece4e75b91cfcb
fr0ntenddev <fr0ntenddev@rage.trade>
2022-10-13 22:48:46 +0530

    feat: implement deposit withdraw for senior vault

---
+++ rage/src/mappings/DnGMX/DnGmxSeniorVault.ts
@@ -0,0 +1,178 @@
+import { log } from '@graphprotocol/graph-ts';
+import {
+  Deposit,
+  Withdraw,
+} from '../../../generated/DnGmxSeniorVault/DnGmxSeniorVault';
+import { VaultDepositWithdrawEntry } from '../../../generated/schema';
+import {
+  BigIntToBigDecimal,
+  safeDiv,
+  generateId,
+} from '../../utils';
+import { contracts } from '../../utils/addresses';
+import { BI_18, BI_6, ONE_BD, ONE_BI } from '../../utils/constants';
+import {
+  updateEntryPrices_deposit,
+  updateEntryPrices_withdraw,
+} from '../../utils/entry-price';
+import { getERC20Token } from '../../utils/getERC20Token';
+import { getVault } from '../../utils/getVault';
+import { getOwner } from '../clearinghouse/owner';
+
+export function handleDeposit(event: Deposit): void {
+  log.debug(
+    'custom_logs: handleDeposit triggered [ caller - {} ] [ owner - {} ] [ asset - {} ] [ shares - {} ]',
+    [
+      event.params.caller.toHexString(),
+      event.params.owner.toHexString(),
+      event.params.assets.toString(),
+      event.params.shares.toString(),
+    ]
+  );
+
+  let vault = getVault(contracts.DnGmxSeniorVault);
+  let owner = getOwner(event.params.owner);
+  let token = getERC20Token(contracts.USDC);
+
+  let sharesInBigDecimal = BigIntToBigDecimal(event.params.shares, BI_18);
+  let assetsInBigDecimal = BigIntToBigDecimal(event.params.assets, BI_6); // asset is USDC
+
+  let assetsPerShare = safeDiv(assetsInBigDecimal, sharesInBigDecimal);
+
+  let assetPrice = ONE_BD; // 1 USDC = 1 dollar
+  let sharePrice = assetPrice.times(assetsPerShare);
+
+  updateEntryPrices_deposit(
+    event.params.owner,
+    contracts.DnGmxSeniorVault,
+    assetsInBigDecimal,
+    sharesInBigDecimal,
+    assetPrice,
+    sharePrice
+  );
+
+  log.debug(
+    'custom_logs: handleDeposit owner - {} sharePrice - {} sharesInBigDecimal - {}',
+    [
+      event.params.owner.toHexString(),
+      sharePrice.toString(),
+      sharesInBigDecimal.toString(),
+    ]
+  );
+
+  owner.save();
+
+  //...........................................................................//
+
+  let vaultDepositWithdrawEntryId = generateId([
+    vault.id,
+    owner.id,
+    event.params.owner.toHexString(),
+    event.block.number.toHexString(),
+    event.logIndex.toHexString(),
+  ]);
+
+  let entry = new VaultDepositWithdrawEntry(vaultDepositWithdrawEntryId);
+
+  entry.timestamp = event.block.timestamp;
+  entry.blockNumber = event.block.number;
+  entry.transactionHash = event.transaction.hash;
+
+  entry.owner = owner.id;
+  entry.vault = vault.id;
+  entry.token = token.id;
+
+  entry.action = 'deposit';
+
+  entry.assetsTokenAmount = assetsInBigDecimal;
+  entry.tokenAmount = entry.assetsTokenAmount;
+
+  entry.sharesTokenAmount = sharesInBigDecimal;
+  entry.sharesTokenDollarValue = entry.sharesTokenAmount.times(sharePrice);
+
+  entry.assetPrice = assetPrice;
+  entry.sharePrice = sharePrice;
+
+  owner.vaultDepositWithdrawEntriesCount = owner.vaultDepositWithdrawEntriesCount.plus(
+    ONE_BI
+  );
+  owner.save();
+  entry.save();
+}
+
+export function handleWithdraw(event: Withdraw): void {
+  log.debug(
+    'custom_logs: handleWithdraw triggered [ caller - {} ] [ receiver - {} ] [ owner - {} ] [ asset - {} ] [ shares - {} ]',
+    [
+      event.params.caller.toHexString(),
+      event.params.receiver.toHexString(),
+      event.params.owner.toHexString(),
+      event.params.assets.toString(),
+      event.params.shares.toString(),
+    ]
+  );
+
+  let vault = getVault(contracts.DnGmxSeniorVault);
+  let owner = getOwner(event.params.owner);
+  let token = getERC20Token(contracts.USDC);
+
+  let sharesInBigDecimal = BigIntToBigDecimal(event.params.shares, BI_18);
+  let assetsInBigDecimal = BigIntToBigDecimal(event.params.assets, BI_6); // asset is USDC
+
+  //...........................................................................//
+
+  updateEntryPrices_withdraw(
+    event.params.owner,
+    contracts.DnGmxSeniorVault,
+    assetsInBigDecimal,
+    sharesInBigDecimal
+  );
+
+  log.debug('custom_logs: handleWithdraw owner - {} sharesInBigDecimal - {}', [
+    event.params.owner.toHexString(),
+    sharesInBigDecimal.toString(),
+  ]);
+
+  //...........................................................................//
+
+  let vaultDepositWithdrawEntryId = generateId([
+    vault.id,
+    owner.id,
+    event.params.receiver.toHexString(),
+    event.block.number.toHexString(),
+    event.logIndex.toHexString(),
+  ]);
+
+  let entry = new VaultDepositWithdrawEntry(vaultDepositWithdrawEntryId);
+
+  entry.timestamp = event.block.timestamp;
+  entry.blockNumber = event.block.number;
+  entry.transactionHash = event.transaction.hash;
+
+  entry.owner = owner.id;
+  entry.vault = vault.id;
+  entry.token = token.id;
+
+  entry.action = 'withdraw';
+
+  entry.assetsTokenAmount = BigIntToBigDecimal(event.params.assets, BI_6);
+  entry.tokenAmount = entry.assetsTokenAmount;
+
+  entry.sharesTokenAmount = sharesInBigDecimal;
+
+  let assetsPrice = ONE_BD; // 1 USDC = 1 dollar;
+
+  entry.assetPrice = assetsPrice;
+  entry.sharePrice = safeDiv(
+    assetsPrice.times(entry.assetsTokenAmount),
+    entry.sharesTokenAmount
+  );
+
+  entry.sharesTokenDollarValue = entry.assetsTokenAmount.times(assetsPrice);
+
+  owner.vaultDepositWithdrawEntriesCount = owner.vaultDepositWithdrawEntriesCount.plus(
+    ONE_BI
+  );
+  owner.save();
+  entry.save();
+}
