d9345948b7a77c1963dba4e238791e4c7f60f1d3
fr0ntenddev <fr0ntenddev@rage.trade>
2022-11-11 15:25:22 +0530

    feat: update logic for handleDepositToken and handleBatchDeposit for dn-gmx batching manager

--- rage/src/mappings/DnGMX/DnGmxBatchingManager.ts
+++ rage/src/mappings/DnGMX/DnGmxBatchingManager.ts
@@ -1,28 +1,26 @@
-import { Address, log, BigInt } from '@graphprotocol/graph-ts';
+import { Address, log } from '@graphprotocol/graph-ts';
 import { Owner, VaultDepositWithdrawEntry } from '../../../generated/schema';
 import {
   BatchDeposit,
   DepositToken,
 } from '../../../generated/DnGmxBatchingManager/DnGmxBatchingManager';
-import { DnGmxJuniorVault } from '../../../generated/DnGmxJuniorVault/DnGmxJuniorVault';
-import { BigIntToBigDecimal, generateId, parsePriceX128, safeDiv } from '../../utils';
+import { BigIntToBigDecimal, generateId, safeDiv } from '../../utils';
 import { contracts } from '../../utils/addresses';
 import { getVault } from '../../utils/getVault';
 import { getOwner } from '../clearinghouse/owner';
 import { getERC20Token } from '../../utils/getERC20Token';
-import { BI_18, BI_6, ONE_BI, ZERO_BD, ZERO_BI } from '../../utils/constants';
+import { BI_18, BI_6, ONE_BI, ZERO_BD } from '../../utils/constants';
 import { updateEntryPrices_deposit } from '../../utils/entry-price';
 
 // only for the DnGmxJuniorVault, which also allows deposit of USDC
 
 export function handleDepositToken(event: DepositToken): void {
   log.debug(
-    'custom_logs: handleDepositToken triggered [ token - {} ] [ receiver - {} ] [ amount - {} ] [ glpStaked - {} ]',
+    'custom_logs: handleDepositToken triggered [ token - {} ] [ receiver - {} ] [ amount - {} ]',
     [
       event.params.token.toHexString(),
       event.params.receiver.toHexString(),
-      event.params.amount.toString(),
-      event.params.glpStaked.toString(),
+      event.params.amount.toString(), // USDC amount
     ]
   );
 
@@ -50,20 +48,13 @@ export function handleDepositToken(event: DepositToken): void {
 
   entry.action = 'deposit';
 
-  let dnGmxJuniorVaultContract = DnGmxJuniorVault.bind(contracts.DnGmxJuniorVault);
+  entry.tokenAmount = BigIntToBigDecimal(event.params.amount, token.decimals); // USDC
 
-  entry.tokenAmount = BigIntToBigDecimal(event.params.amount, token.decimals);
-  entry.assetsTokenAmount = BigIntToBigDecimal(event.params.glpStaked, BI_18);
-  entry.sharesTokenAmount = BigIntToBigDecimal(ZERO_BI, BI_18); // TODO update shares from batch deposit
-
-  let assetPrice = event.params.token.equals(contracts.USDC)
-    ? safeDiv(entry.tokenAmount, entry.assetsTokenAmount)
-    : parsePriceX128(dnGmxJuniorVaultContract.getPriceX128(), BI_18, BI_6);
-
-  entry.assetPrice = assetPrice;
+  entry.assetsTokenAmount = ZERO_BD;
+  entry.sharesTokenAmount = ZERO_BD;
+  entry.assetPrice = ZERO_BD;
   entry.sharePrice = ZERO_BD;
-
-  entry.sharesTokenDollarValue = entry.assetsTokenAmount.times(assetPrice);
+  entry.sharesTokenDollarValue = ZERO_BD;
 
   owner.vaultDepositWithdrawEntriesCount = owner.vaultDepositWithdrawEntriesCount.plus(
     ONE_BI
@@ -73,18 +64,19 @@ export function handleDepositToken(event: DepositToken): void {
   let pendingDeposits = vault.pendingDeposits; // copy to mem
   pendingDeposits.push(entry.id);
   vault.pendingDeposits = pendingDeposits;
-  vault.save();
 
+  vault.save();
   owner.save();
   entry.save();
 }
 
 export function handleBatchDeposit(event: BatchDeposit): void {
   log.debug(
-    'custom_logs: handleBatchDeposit_DN_GMX_Vault triggered [ round - {} ] [ userGlpAmount - {} ] [ userShareAmount - {} ]',
+    'custom_logs: handleBatchDeposit_DN_GMX_Vault triggered [ round - {} ] [ userUsdcAmount - {}] [ userGlpAmount - {} ] [ userShareAmount - {} ]',
     [
       event.params.round.toHexString(),
-      event.params.userGlpAmount.toHexString(),
+      event.params.userUsdcAmount.toString(),
+      event.params.userGlpAmount.toString(),
       event.params.userShareAmount.toString(),
     ]
   );
@@ -92,11 +84,6 @@ export function handleBatchDeposit(event: BatchDeposit): void {
   let vault = getVault(contracts.DnGmxJuniorVault);
 
   let pendingDeposits = vault.pendingDeposits; // copy to mem
-
-  log.debug(
-    'custom_logs: handleBatchDeposit_DN_GMX_Vault - pendingDeposits.length - {}',
-    [BigInt.fromI32(pendingDeposits.length).toString()]
-  );
 
   for (let i = 0; i < pendingDeposits.length; i++) {
     let entry = VaultDepositWithdrawEntry.load(pendingDeposits[i]);
@@ -106,17 +93,21 @@ export function handleBatchDeposit(event: BatchDeposit): void {
         []
       );
     } else {
-      entry.sharesTokenAmount = safeDiv(
-        entry.assetsTokenAmount.times(
-          BigIntToBigDecimal(event.params.userShareAmount, BI_18)
-        ),
-        BigIntToBigDecimal(event.params.userGlpAmount, BI_18)
+      entry.sharesTokenAmount = entry.tokenAmount.times(
+        safeDiv(
+          BigIntToBigDecimal(event.params.userShareAmount, BI_18),
+          BigIntToBigDecimal(event.params.userUsdcAmount, BI_6)
+        )
       );
 
-      entry.sharePrice = safeDiv(
-        entry.assetPrice.times(entry.assetsTokenAmount),
-        entry.sharesTokenAmount
+      entry.assetsTokenAmount = entry.tokenAmount.times(
+        safeDiv(
+          BigIntToBigDecimal(event.params.userGlpAmount, BI_18),
+          BigIntToBigDecimal(event.params.userUsdcAmount, BI_6)
+        )
       );
+
+      entry.sharesTokenDollarValue = entry.tokenAmount;
 
       entry.save();
 
